73b849de3e755ab6084c5676e2b169a856d8b8fd
VincentShen <wendong.shen@1hai.cn>
2016-03-24 18:20:58 +0800

    Add EscapeText.Wpf Project

---
+++ EscapeText.Wpf/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Newtonsoft.Json;
+
+namespace EscapeText.Wpf
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void SubmitBtn_Click(object sender, RoutedEventArgs e)
+        {
+            var rawText = JsonConvert.SerializeObject(this.RawTextBox.Text);
+            this.EscapedTextBox.Text = rawText;
+        }
+    }
+}
